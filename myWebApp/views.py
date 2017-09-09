b2f827f43f06fafd4341ce1d4cd6de9222dcbbaa
zeffon <zephoony#gmail.com>
2017-09-09 12:01:05 +0800

    按照django官方文档所写，1-7（省去5测试环节）

---
+++ myWebApp/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+# Create your views here.
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'myWebApp/index.html', context)
+
+
+def detail(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myWebApp/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myWebApp/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'myWebApp/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('myWebApp:results', args=(question.id,)))
+
+
+class IndexView(generic.ListView):
+    template_name = 'myWebApp/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'myWebApp/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'myWebApp/results.html'
